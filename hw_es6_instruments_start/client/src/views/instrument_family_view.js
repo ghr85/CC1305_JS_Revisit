7f327898aa2a4c2cc6f7cc58cc0fd0bdf3de40cd
ghr85 <ghrenfrew@gmail.com>
2019-04-12 14:11:27 +0100

    created class for view

--- hw_es6_instruments_start/client/src/views/instrument_family_view.js
+++ hw_es6_instruments_start/client/src/views/instrument_family_view.js
@@ -1,17 +1,18 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const InstrumentFamilyView = function (container) {
+class InstrumentFamilyView{
+constructor(container){
   this.container = container;
 };
 
-InstrumentFamilyView.prototype.bindEvents = function () {
+bindEvents() {
   PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) => {
     const instrumentFamily = evt.detail;
     this.render(instrumentFamily);
   });
 };
 
-InstrumentFamilyView.prototype.render = function (family) {
+render(family) {
   this.container.innerHTML = '';
 
   const familyName = this.createElement('h2', family.name);
@@ -27,13 +28,13 @@ InstrumentFamilyView.prototype.render = function (family) {
   this.container.appendChild(instrumentList);
 };
 
-InstrumentFamilyView.prototype.createElement = function (elementType, text) {
+createElement(elementType, text) {
   const element = document.createElement(elementType);
   element.textContent = text;
   return element;
 };
 
-InstrumentFamilyView.prototype.createInstrumentList = function (instruments) {
+createInstrumentList(instruments) {
   const list = document.createElement('ul');
 
   instruments.forEach((instrument) => {
@@ -43,6 +44,7 @@ InstrumentFamilyView.prototype.createInstrumentList = function (instruments) {
   });
 
   return list;
+};
 };
 
 module.exports = InstrumentFamilyView;
